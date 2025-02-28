3e8bd0c1b9ade609ac658a5118a9110614c7c1db
Aditya-tech1 <adityatech1m@gmail.com>
2025-03-01 00:15:14 +0530

    aditya

--- client/src/app/shop/page.jsx
+++ client/src/app/shop/page.jsx
@@ -1,6 +1,6 @@
+import { WhatsAppIcon } from '@/icons/icon';
 import Link from 'next/link';
 import React from 'react';
-import Link from 'next/link';
 
 const products = [
   { name: "Paper", price: "₹150", quantity: "5kg", link: "/products/paper", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKr7KLRYm614imBxNtYkLLjl1vfL9XgnsqKzHahAdByRTB7RvZw-oXjMA&s=10" },
@@ -11,14 +11,14 @@ const products = [
   { name: "Metal", price: "₹650", quantity: "2kg", link: "/products/metal", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2W46QDQTxPWvO1VpJ9gRyaUyoFIdSF17pwQ&usqp=CAU" },
   { name: "Fabric", price: "₹350", quantity: "3kg", link: "/products/fabric", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB4RbDtfqI_UpcBCPTjqfqjRhOBI-1BdXzhg&usqp=CAU" },
   { name: "Cotton", price: "₹180", quantity: "2kg", link: "/products/cotton", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-mjg3WvHUD_svkkH6TXNfdz24XDD2vWg1Jw&usqp=CAU" },
-  { name: "E-waste", price: "₹500", quantity: "1kg", link: "/products/e-waste", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQbvZ2tN5uBhSxIj6_-Dc8mqP0mFZebygsbClSt6xizaraR-pbK5XTa9f&s=10" },
+  { name: "E-waste", price: "₹500", quantity: "1kg", link: "/products/e-waste", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0tnhWTJoBqWn3j0Yg6UyKOEtxT8klCvKhCl0V-52Ql9GbuiUBvaqsTncH&s=10" },
 ];
 
 function Page() {
   return (
     <div
-      className="relative w-full h-screen bg-cover bg-center"
-    //style={{ backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQbvZ2tN5uBhSxIj6_-Dc8mqP0mFZebygsbClSt6xizaraR-pbK5XTa9f&s=10)' }} 
+      className="relative w-full h-full bg-cover bg-center"
+    style={{ backgroundImage: 'linear-gradient(#0005, #0005),url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvQbvZ2tN5uBhSxIj6_-Dc8mqP0mFZebygsbClSt6xizaraR-pbK5XTa9f&s=10)', backgroundAttachment: 'fixed' }} 
     >
 
 
@@ -33,20 +33,19 @@ function Page() {
 
 const ProductCard = ({ name, quantity, link, image }) => {
   return (
-    <div className="w-72 bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl">
-      <Link href={link}>
+    <Link href={link} className="w-72 pt-4 bg-white rounded-lg shadow-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-2xl">
         <img src={image} alt={name} className="w-72 h-32 object-contain" />
         <div className="p-3 w-full">
           <h3 className="text-lg font-semibold text-gray-800">{name}</h3>
           <p className="text-sm text-gray-500">Available Quantity: {quantity}</p>
 
 
-          <Link href={link} className="mt-3 w-full py-2 bg-green-600 text-white font-semibold rounded-lg hover:bg-green-700 transition-colors">
-            Enquiry
-          </Link>
+          <button  className="mt-3 w-full py-2 flex flex-row items-center justify-center gap-2 bg-green-600 text-white font-semibold rounded-lg hover:bg-green-700 transition-colors">
+            <WhatsAppIcon className='w-8 h-8'/>
+            Enquiry Now
+          </button>
         </div>
-      </Link>
-    </div>
+    </Link>
   );
 }
 
